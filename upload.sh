ec6bd60f45391f222be414cb7925272f260d61d3
928291152@qq.com <928291152@qq.com>
2022-02-09 14:41:18 +0800

    js更新

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 url_prefix="old/"
 cd /data/git/whatsapp
-node override_script.js $url_prefix$1 $url_prefix$2 >> log.txt 2>&1
+node override_script.js $1 $2 >> log.txt 2>&1
 sleep 5s
 git add -A
 git commit -m"js更新"
